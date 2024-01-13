c8763154defa66ded6313f79d0ec6af64f06f75f
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2024-01-13 16:47:28 -0500

    Made the bare-bones for the ErrorPage page, which shoul popup whenever there's an invalid link, and it directs the user to the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import './stylesheet/App.css'
 import Home from './components/Home.tsx';
 import AddEmployee from './components/AddEmployee.tsx';
+import ErrorPage from './components/ErrorPage.tsx';
 
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   
   return (
-    <>
     <Router>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Employee Routes */}
-        <Route path="add-employee" element={<AddEmployee />} />
-        {/* <Route path="employee" element={<SingleEmployeeView />} /> */}
-        {/* <Route path="*" element={<NoPage />} /> */}
-      </Routes>
+      <div>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            {/* Employee Routes */}
+            <Route path='add-employee' element={<AddEmployee />} />
+            {/* <Route path="employee" element={<SingleEmployeeView />} /> */}
+            <Route path='*' element={<ErrorPage />} />
+          </Routes>
+      </div>
     </Router>
-    
-    </>
   );
 };
 
