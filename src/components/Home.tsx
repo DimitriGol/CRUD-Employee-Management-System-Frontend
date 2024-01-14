b749a5e8f37e23c48d94448ed4314effe5065a51
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2024-01-14 17:28:11 -0500

    Make the Home component more like a landing page. Removed the employee table and added it to the Employees component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,25 @@
 import { Button } from "flowbite-react"
 import { Link } from "react-router-dom"
-import EmployeeTable from "./EmployeeTable"
+// import Empl from "./EmployeeTable"
 
 function Home() {
-    return (
+    return (    
         <>
-            <div className="px-4 py-2 m-2 text-center">
+            <div className="px-4 py-2 m-2 mt-14 text-center">
                 <div className="">
-                    <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-6xl">Employee CRUD Management System</h1>
+                    <h1 className="mb-3 text-4xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-6xl">Employee <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-500">CRUD</span> Management System</h1>
+                    <p className="mb-5 text-xl font-extralight text-gray-500">
+                        Manage your employee database with this CRUD management system.
+                    </p>
                 </div>
-                
-                <EmployeeTable/>
-                <div className="flex justify-center w-90">
-                    <Link to='/add-employee'>
-                        <Button className="mt-6 py-2" gradientDuoTone="purpleToBlue">Add New Employee</Button>
+                <div className="flex justify-center">
+                    <Link to='/employees'>
+                        <Button className="flex-auto mt-6 mr-2 py-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 " gradientDuoTone="purpleToBlue">View All Employees</Button>
+                    </Link>
+                    <Link to='#'>
+                        <Button className="flex-auto mt-6 ml-2 py-2 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 " gradientDuoTone="purpleToBlue">View All Tasks</Button>
                     </Link>
                 </div>
-                
             </div>
         </>
     );
