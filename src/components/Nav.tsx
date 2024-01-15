cb3b803539d316aeea56b139099db5b65edc40fb
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2024-01-14 22:19:05 -0500

    Add Toggle option for the links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,6 +7,7 @@ function Nav() {
                 <Navbar.Brand>
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Employee CRUD Management System</span>
                 </Navbar.Brand>
+                <Navbar.Toggle />
                 <Navbar.Collapse>
                     <Navbar.Link href="#" active>
                         Home
